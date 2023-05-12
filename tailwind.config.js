6d561d9434ff3e4dea3de710e84bd2326e28706b
Dominic van Almsick <dominicvana@gmail.com>
2023-05-12 19:55:47 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'base-100': '#ffffff',
+        'base-200': '#F5F5F5',
+        'base-300': '#E4E4E4',
+        'base-400': '#C1C4CB',
+        'base-500': '#7C8187',
+        'base-600': '#5A6069',
+        'base-700': '#35393F',
+        'base-800': '#2B2D31',
+        'base-900': '#1D1F22',
+        'base-1000': '#151619',
+        primary: '#E46643',
+        secondary: '#F39765',
       },
     },
   },
   plugins: [],
-}
+};
