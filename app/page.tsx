28f0b9928b928b0a96b3cacb433be73b04b08333
Dominic van Almsick <dominicvana@gmail.com>
2023-05-12 20:40:10 +0100

    feat: init header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import { Roboto } from 'next/font/google';
+import { AiOutlineMenu, AiOutlineFile, AiOutlineSave } from 'react-icons/ai';
+import { FaRegTrashAlt } from 'react-icons/fa';
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-base-500">Hello, Next.js</h1>
-    </main>
+    <>
+      <header className="flex items-center bg-base-800 pr-2 text-base-100">
+        <button className="mr-6 flex h-14 w-14 items-center justify-center bg-base-700">
+          <AiOutlineMenu />
+        </button>
+        <div className="mr-auto flex items-center gap-2">
+          <AiOutlineFile />
+          <h1 className={roboto.className}>untitled.md</h1>
+        </div>
+        <button className="mr-6">
+          <FaRegTrashAlt className="text-base-500" />
+        </button>
+        <button className="flex h-10 w-10 items-center justify-center rounded bg-primary">
+          <AiOutlineSave />
+        </button>
+      </header>
+      <main>
+        <h1 className="text-base-500">Markdown Editor</h1>
+      </main>
+    </>
   );
 }
