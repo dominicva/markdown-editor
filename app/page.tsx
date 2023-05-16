0e159150fbbf59e240ab9ed6174d7ccf6f7c45d4
Dominic van Almsick <dominicvana@gmail.com>
2023-05-16 14:16:45 +0100

    feat: toggle preview transition

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,32 @@
 import { Roboto } from 'next/font/google';
-import {
-  AiOutlineMenu,
-  AiOutlineFile,
-  AiOutlineSave,
-  AiOutlineEye,
-} from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineFile, AiOutlineSave } from 'react-icons/ai';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import Panes from '@/components/Panes';
+
 const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
   return (
     <main>
-      <header className="flex items-center bg-base-800 pr-2 text-base-100">
-        <button className="mr-6 flex h-14 w-14 items-center justify-center bg-base-700">
-          <AiOutlineMenu />
-        </button>
-        <div className="mr-auto flex items-center gap-2">
-          <AiOutlineFile />
-          <h2 className={roboto.className}>untitled.md</h2>
-        </div>
-        <button className="mr-6">
-          <FaRegTrashAlt className="text-base-500" />
-        </button>
-        <button className="flex h-10 w-10 items-center justify-center rounded bg-primary">
-          <AiOutlineSave />
-        </button>
-      </header>
-      <div className="flex items-center justify-between bg-base-200 p-4">
-        <h2
-          className={`${roboto.className} text-sm uppercase tracking-wide text-base-500`}
-        >
-          markdown editor
-        </h2>
-        <button>
-          <AiOutlineEye className="h-6 w-6" />
-        </button>
+      <div className="fixed w-full">
+        <header className="flex items-center bg-base-800 pr-2 text-base-100">
+          <button className="mr-6 flex h-14 w-14 items-center justify-center bg-base-700">
+            <AiOutlineMenu />
+          </button>
+          <div className="mr-auto flex items-center gap-2">
+            <AiOutlineFile />
+            <h2 className={`${roboto.className} mb-0 text-sm`}>untitled.md</h2>
+          </div>
+          <button className="mr-6">
+            <FaRegTrashAlt className="text-base-500" />
+          </button>
+          <button className="flex h-10 w-10 items-center justify-center rounded bg-primary">
+            <AiOutlineSave />
+          </button>
+        </header>
       </div>
 
-      <section>
-        {/* <MarkdownPane /> */}
-
+      <section className="overflow-hidden pt-14">
         <Panes />
       </section>
     </main>
--- components/Panes.tsx
+++ components/Panes.tsx
@@ -3,13 +3,21 @@
 import { ChangeEvent, ChangeEventHandler, useState } from 'react';
 import { marked } from 'marked';
 
-import { Roboto_Mono } from 'next/font/google';
+import { Roboto_Mono, Roboto } from 'next/font/google';
+import { AiOutlineEye } from 'react-icons/ai';
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
 export default function Panes() {
   const [markdown, setMarkdown] = useState('# Welcome to Markdown');
+  const [isPreview, setIsPreview] = useState(false);
+
   const html = marked.parse(markdown);
+
+  function handlePreviewToggle() {
+    setIsPreview(!isPreview);
+  }
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setMarkdown(e.target.value);
@@ -17,8 +25,22 @@ export default function Panes() {
 
   return (
     <>
-      <MarkdownPane markdown={markdown} onChange={handleChange} />
-      <HtmlPane html={html} />
+      <div className="flex items-center justify-between bg-base-200 p-4">
+        <h2
+          className={`${roboto.className} mb-0 text-sm uppercase tracking-wide text-base-500`}
+        >
+          markdown editor
+        </h2>
+        <button onClick={handlePreviewToggle}>
+          <AiOutlineEye className="h-6 w-6" />
+        </button>
+      </div>
+      <MarkdownPane
+        markdown={markdown}
+        onChange={handleChange}
+        isPreview={isPreview}
+      />
+      <HtmlPane html={html} isPreview={isPreview} />
     </>
   );
 }
@@ -26,23 +48,30 @@ export default function Panes() {
 function MarkdownPane({
   markdown,
   onChange,
+  isPreview,
 }: {
   markdown: string;
   onChange: ChangeEventHandler;
+  isPreview: boolean;
 }) {
   return (
     <textarea
-      className={`${robotoMono.className} h-[calc(100vh-112px)] w-full resize-none p-4 text-base-700`}
+      className={`${robotoMono.className} ${
+        isPreview ? '-translate-x-full' : ''
+      } absolute  h-[calc(100vh-112px)] w-full resize-none p-4 text-base-700 transition-transform duration-300 ease-in`}
       value={markdown}
       onChange={onChange}
     ></textarea>
   );
 }
 
-function HtmlPane({ html }: { html: string }) {
+function HtmlPane({ html, isPreview }: { html: string; isPreview: boolean }) {
+  const styles = `${isPreview ? '' : 'translate-x-full'}
+  }h-[calc(100vh-112px)] w-full  resize-none p-4 text-base-700 transition-transform duration-300 ease-in`;
+
   return (
     <article
-      className={`h-[calc(100vh-112px)] w-full resize-none p-4 text-base-700`}
+      className={styles}
       dangerouslySetInnerHTML={{ __html: html }}
     ></article>
   );
